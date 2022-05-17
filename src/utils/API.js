02e87612628308a42b52d09f47f93e3c71942209
Hugo Ernesto <mail.hbler@gmail.com>
2022-05-17 10:11:03 -0300

    update: rotas prontas, iniciando testes

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,7 +2,7 @@ class API {
   static token = "";
   static base_URL = "https://api-kenzie-food.herokuapp.com";
 
-  static async loginUsuario() {
+  static async loginUsuario(obj) {
     const url = API.base_URL + "/auth/login";
 
     const res = await fetch(url, {
@@ -100,6 +100,9 @@ class API {
 
     const res = await fetch(url, {
       method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
     })
       .then((res) => res.json())
       .then((res) => res)
@@ -110,11 +113,83 @@ class API {
 
     return res;
   }
-  static async produtosEditar(id, obj) {}
+  static async produtosEditar(id, obj) {
+    const url = API.base_URL + "/my/produtcs/" + id;
 
-  static async carrinhoSalvo() {}
-  static async carrinhoRemover(id) {}
-  static async carrinhoAdicionar(obj) {}
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+      body: JSON.stringify(obj),
+    })
+      .then((res) => res.json())
+      .then((res) => res)
+      .catch((err) => {
+        console.log(err);
+        return err;
+      });
+
+    return res;
+  }
+
+  static async carrinhoSalvo() {
+    const url = API.base_URL + "/cart";
+
+    const res = await fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => res)
+      .catch((err) => {
+        console.log(err);
+        return err;
+      });
+
+    return res;
+  }
+  static async carrinhoRemover(id) {
+    const url = API.base_URL + "/cart/remove" + id;
+
+    const res = await fetch(url, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => res)
+      .catch((err) => {
+        console.log(err);
+        return err;
+      });
+
+    return res;
+  }
+  static async carrinhoAdicionar(obj) {
+    const url = API.base_URL + "/cart/add";
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+      body: JSON.stringify(obj),
+    })
+      .then((res) => res.json())
+      .then((res) => res)
+      .catch((err) => {
+        console.log(err);
+        return err;
+      });
+
+    return res;
+  }
 }
 
 export default API;
