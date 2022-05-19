ce93c91ca93d6fd8f22d907c6ddbd996515d2783
Hugo Ernesto <mail.hbler@gmail.com>
2022-05-19 19:26:50 -0300

    update: modais e funcionalidades funcionando, editar está com um bug

--- src/script.js
+++ src/script.js
@@ -1,8 +1,15 @@
-import ControllerVitrine from "./controllers/ControllerVitrine.js";
-import ControllerFiltros from "./controllers/ControllerFiltros.js";
+import ControllerProdutos from "./controllers/ControllerProdutos.js";
+import ControllerFormulario from "./controllers/ControllerFormulario.js";
 
-ControllerVitrine.mostrarTodos();
-await ControllerFiltros.criarSelecao();
+localStorage.setItem(
+  "token",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjMyMjNhYTM2LTE2YTItNDc0OC1iNjY0LTkyZmRhOGUyYTVlYSIsImlhdCI6MTY1Mjk5Mzc4NiwiZXhwIjoxNjUzODU3Nzg2LCJzdWIiOiJbb2JqZWN0IFVuZGVmaW5lZF0ifQ.d-hCl6K0CP9VCM9sPgpn0V2Pt2yF1svytcM59sEee1E"
+);
 
-const input = document.getElementById("pesquisar");
-input.addEventListener("keydown", ControllerFiltros.pesquisar);
+const btn1 = document.querySelector(".cadastrar");
+const btn2 = document.querySelector(".editar");
+
+btn1.addEventListener("click", () => {
+  ControllerFormulario.produtoUsuario("cadastrar");
+});
+btn2.addEventListener("click", ControllerProdutos.editar);
