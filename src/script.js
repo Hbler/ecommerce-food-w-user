2ba3938ecdb6cb15ff4c01875df282d5e67259fc
Hugo Ernesto <mail.hbler@gmail.com>
2022-05-20 02:30:54 -0300

    update: branch preparada para merge

--- src/script.js
+++ src/script.js
@@ -1,4 +1,1 @@
-import ControllerFormulario from "./controllers/ControllerFormulario.js";
 
-const teste = document.getElementById("teste");
-teste.addEventListener("click", ControllerFormulario.formularioLogin);
